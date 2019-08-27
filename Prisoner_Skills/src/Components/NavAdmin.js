c7e8e164a4e24eba2a98dc77741d864aa76bd0d8
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-27 04:47:04 +0100

    WIP: added placeholder logo

--- Prisoner_Skills/src/Components/NavAdmin.js
+++ Prisoner_Skills/src/Components/NavAdmin.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Menu, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 import history from './../utils/history'
+import logo from './../img/logo.png'
+
+const StyledLogo = styled.img`
+  height: 40px;
+`
 
 const NavAdmin = props => {
   const signOutHandler = event => {
@@ -14,6 +20,11 @@ const NavAdmin = props => {
 
   return (
     <Menu size='big'>
+      <Menu.Item position='left'>
+        <Link to="/">
+          <StyledLogo src={logo} alt='Prisoner Skills logo'/>
+        </Link>
+      </Menu.Item>
       <Menu.Item  position='right'>
         <NavLink 
           to="/admin/prisoner/new" 
