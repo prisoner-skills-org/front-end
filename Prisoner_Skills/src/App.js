397ed0fc40c10934f6c5634b0aee1b79de8c6845
Mike Harley <mharley12345@gmail.com>
2019-08-27 12:49:38 -0700

    signUp and login

--- Prisoner_Skills/src/App.js
+++ Prisoner_Skills/src/App.js
@@ -1,12 +1,13 @@
 import React  from 'react'
-import FormikForm from './Components/Login'
-
+// import FormikForm from './Components/Login'
+import FormikForm from './Components/signUp'
+import './App.css'
 
 function App(){
     return(
         <div className='App'>
-  <FormikForm/>
-
+  {/* <FormikForm/> */}
+<FormikForm/>
 
         </div>
     )
