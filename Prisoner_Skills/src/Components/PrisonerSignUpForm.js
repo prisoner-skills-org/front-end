d5c11f0199e2c61fb05be813400e8d5cea6784fc
Mike Harley <mharley12345@gmail.com>
2019-08-28 17:36:28 -0700

    Style and fixes onSubmit prisonerSignUpForm

--- Prisoner_Skills/src/Components/PrisonerSignUpForm.js
+++ Prisoner_Skills/src/Components/PrisonerSignUpForm.js
@@ -1,29 +1,62 @@
-import AddSkill from "./AddSkill"
+// import AddSkill from "./AddSkill"
 import  React,{useEffect,useState} from "react";
 import {Form, Field,withFormik} from "formik";
 import * as Yup from 'yup';
 import axios from  'axios'
 import styled from 'styled-components';
+import {
+  Checkbox,
+  Grid,
+  Header,
+  Segment,
+  Form as SemanticForm,
+ 
+  TextArea,} from 'semantic-ui-react'
+  import '../App.css'
 
-const Flex = styled.div`
-display:flex;
-flex-direction:column;
-flex-wrap:wrap;
-text-align:center;
-width:960px;
-height:800px;
-background-color: #007bff;
-padding-top:10rem;
-margin-left:10em;`
 
-const Title = styled.h1`
-font-size:80px;`
-const Label = styled.label`
+
+
+const FormContainer = styled.div`
+  height: 95vh;
+  width: 100vw;
+  background-color: #F7F7F7;
+`
+const FieldContainer = styled.div`
+  height: 170px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+const StyledErrorMessage = styled.p`
+  color: red;
+  margin: 10px;
+`
+const Button = styled.button`
+background-color:white;
+width:  150px;
+height:50px;
+border-radius:12px;
+margin-left:12rem;
+
+margin-top:3rem;
+
+&:hover{
+  background-color:#007bff;
+  color:white;
+}`
+
+
+
+const Yes = styled.div` 
+font-size:20px;
 `
 
- function FormBuilder({values,errors,touched,status}){
+
+
+ function FormBuilder({value,errors,touched,status}){
     const[newUser,setUser] =useState([])
- 
+
     useEffect(() => {
     if (status) {
       setUser([...newUser, status ])
@@ -32,40 +65,50 @@ const Label = styled.label`
     }, [status]);
     
  return (
-  
-   <Flex className="SignUp" id="PrisonerSignUp">
-     
-     <Title>Add Inmate</Title>
-    
-     <Form>
-  
-   <Label><h3>Inmates Name</h3>
-
-       <Field type="name" name="name" placeholder="name"/>
-   </Label>
-   
-     
-      
-          
-       <Label className="checkbox-container">
-          <h2>Outside Clearance ?</h2>
-          <Field
-            type="checkbox"
-            name="cleared"
-     
-          />
-    
-      
-        </Label>
-
-      
-  
-     </Form>
-   
-      <AddSkill/>
+  <FormContainer>
  
-
-       </Flex>
+     <Form>
+       <Grid  textAlign='center' style={{ height: '70vh' }} verticalAlign='middle'>
+       <Grid.Column style={{ maxWidth: 600 }}>
+     <Header as ='h2' color='white' textAlign='center'>
+       Add Inmate
+       </Header>
+    <SemanticForm size ='large'>
+      <Segment stacked>
+        
+        <FieldContainer>
+          <Header as ='h3'>Inmates Name {<br/>}
+        <Field type="name" name="name" />
+        </Header>
+        </FieldContainer>
+         <FieldContainer>
+         <Header as='h3'>Cleared For Outside?</Header>
+        <Yes> Yes </Yes>
+        <Checkbox  type="checkbox" name="cleared" />
+         </FieldContainer>
+               
+ 
+       
+       
+        
+        <FieldContainer>
+          <Header as='h3'>Skills:
+       
+         <Field type='textarea' name='skills' placeholder="Add Skills"/>
+         </Header>
+        </FieldContainer>
+        <FieldContainer>
+          <Button type = 'submit'>Submit</Button>
+      </FieldContainer>
+      </Segment>
+      </SemanticForm>
+     
+      {touched.name && errors.name && <StyledErrorMessage>{errors.name}</StyledErrorMessage>} 
+      </Grid.Column>
+      </Grid>
+    </Form>
+     
+       </FormContainer>
   )
 };
    
@@ -75,14 +118,14 @@ const Label = styled.label`
 
 
 const FormikSign = withFormik({
-    mapPropsToValues({name,cleared,skills,id}){
+    mapPropsToValues({name,skills}){
         return{
               
-             id:id ||"",      
-           user:name || "",
-            cleared:cleared || "",
+              
+           name: name || '',
           
-            skills:skills ||"",
+          //  cleared:cleared|| true,
+            skills:skills || '',
                  
             
         }
@@ -92,23 +135,25 @@ const FormikSign = withFormik({
           
      
          name: Yup.string().required("Please Enter A Name"),
-        cleared: Yup.boolean().required(),
+    
+        skills: Yup.string(),
      
      
      
     }),
-       handleSubmit(values,  {setError,resetForm, setStatus }) {
+       handleSubmit(value,  {resetForm , setError,}) {
        
         axios
-          .post("", values)
+          .post("https://reqres.in/api/users", value)
           .then(res => {
-            setStatus(res.data)
-            resetForm()
             
+            resetForm()
+            console.log(res)
           })
           .catch(err => {
             setError(err)
-            console.log("UH OH,",err); // There was an error creating the data and logs to console
+            console.log("UH OH,",err) // There was an error creating the data and logs to console
+            // resetForm({})
           })
     
  
